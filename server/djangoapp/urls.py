f53879d25a37b3888da2202ab643e61b0351e59b
SiyaRG <siyabongafikcurry30@gmail.com>
2026-02-07 13:05:58 +0200

    3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,7 +10,13 @@ urlpatterns = [
     # # path for registration
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
+    # path for registration
+    path(route='register', view=views.registration, name='registration'),
 
     # path for dealer reviews view
 
