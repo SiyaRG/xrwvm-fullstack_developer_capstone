7c91df8df10db7a53a55ea076d6984225d7b352a
SiyaRG <siyabongafikcurry30@gmail.com>
2026-02-07 12:05:42 +0200

    First Tasks complete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,11 +1,12 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 # from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    
     # # path for registration
 
     # path for login
